c24340178f3c6d18f6fd440ba9ee8aeb9137a926
PaulHancock <paul.hancock@curtin.edu.au>
2020-06-17 11:32:09 +0800

    rename libdir

---
+++ calplots/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0'
+__author__ = ['Paul Hancock',
+              'John Morgan',
+              'Franz Kirsten',
+              'Sammy McSweeney',
+              'Christopher Jordan']
